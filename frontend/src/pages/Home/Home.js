26ed5358dde60eff37d414c4391e88620bbd4ea2
sarath <chandu83319@gmail.com>
2025-02-24 00:00:53 +0530

    added home component and app.js file

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to BabyStep App</h1>
+      {/* Add any additional content for your home page here */}
+    </div>
+  );
+};
+
+export default Home;
