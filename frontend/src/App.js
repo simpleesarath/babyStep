6810e33e9d3bfe50654e9047913b8494f7658155
simpleesarath <88574803+simpleesarath@users.noreply.github.com>
2025-02-24 00:14:58 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BookAppointment from './pages/BookAppointment/BookAppointment'; // Ensure this path is correct
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import BookAppointment from './pages/BookAppointment/BookAppointment';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<h1>Welcome to BabyStep App</h1>} />
-        <Route path="/book" element={<BookAppointment />} />
-       
-      </Routes>
+      <div>
+        {/* Add navigation */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/book">Book Appointment</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book" element={<BookAppointment />} />
+          {/* Add a catch-all route */}
+          <Route path="*" element={<div>Page not found!</div>} />
+        </Routes>
+      </div>
     </Router>
-  
-    
-    
   );
 }
 
